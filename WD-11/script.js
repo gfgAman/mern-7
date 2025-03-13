62c3d7441c321acda34756192179752b41c99bec
Aman Kumar <aman.kumar@geeksforgeeks.org>
2025-03-13 13:32:23 +0530

    add:WD-11

---
+++ WD-11/script.js
@@ -0,0 +1,50 @@
+// for(initialVal;finalVal;inc/dec)
+// 1->100
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) console.log(i,'even');
+// }
+
+// let i = 1
+
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+// var a = 10
+// let b = 'hello'
+// const c = true
+
+// function abc() {
+//     function xyz(x, y) {
+//         return x + y
+//     }
+//     function qwerrty() {
+//         console.log(a,'a');
+//         console.log(b,'b');
+//         console.log(c,'c');
+//     }
+//     console.log(xyz(2, 3))
+
+//     console.log(qwerrty());
+// }
+// abc()
+
+// closure: it is a phenomenon in js in which a function is lexically bounded with variable present in outer scope
+
+// hoisting: it is a pheonomen in js in which a variable or a function can be called before their initialization
+// let and const are also hoisted under temporal deadzone
+
+// temporal deadzone: it is a time period between variable declaration and initialization
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// abc()
+// var a = 10
+// let b = 'hello'
+// const c = 'hii there!!!'
+
+// function abc(){
+//     console.log('this is a function');
+// }
